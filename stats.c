8bbd303d69dc92ec3da79d9e1ba69ce7c379aea9
Prakhar Jain <prakharjain0508@gmail.com>
2019-09-09 12:42:52 -0600

    modfied

--- stats.c
+++ stats.c
@@ -39,7 +39,7 @@ void main() {
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100, 90};
+                                7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
@@ -71,6 +71,7 @@ unsigned char find_median(unsigned char * ptr, unsigned int length) {
 void print_statistics(unsigned char * ptr, unsigned int length) {
    
 }
+
 
 void print_array(unsigned char * ptr, unsigned int length) {
  
